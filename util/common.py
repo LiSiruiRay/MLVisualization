8db771d932abeeb1ca21a7ae7dc4f8515fe6ccee
Sirui Li <slray@nyu.edu>
2024-03-24 16:34:56 +0400

    testing get metadata info

--- util/common.py
+++ util/common.py
@@ -1,3 +1,19 @@
 # Author: ray
 # Date: 3/24/24
 # Description:
+import os
+
+
+def get_proje_root_path() -> str:
+    """
+        This function gets absolute path for the root of the project.
+        Whichever program called this function under wherever, it will always return the correct absolute path
+    """
+
+    # If not running in the root folder, return the absolute folder.
+    # get the directory that the current script is in
+    current_script_directory = os.path.dirname(os.path.realpath(__file__))
+
+    # get the path of the resource directory relative to the current script
+    proje_root_path = os.path.join(current_script_directory, '../')
+    return proje_root_path
--- util/model_meta_info_reading.py
+++ util/model_meta_info_reading.py
@@ -1,3 +1,9 @@
 # Author: ray
 # Date: 3/24/24
-# Description:
+# Description: If later on we developed an interface for generating meta info while training, we add interface here
+from util.common import get_proje_root_path
+
+
+def get_model_list_from_folder_reading():
+    proje_root_path = get_proje_root_path()
+
