4032eb910f0dbce0133366562b0637a613101cac
Sirui Li <slray@nyu.edu>
2024-03-25 17:06:06 +0400

    testing fake meta data

--- front_end/test/test_meta_info_page.py
+++ front_end/test/test_meta_info_page.py
@@ -1,3 +1,17 @@
 # Author: ray
 # Date: 3/25/24
 # Description:
+
+import streamlit as st
+
+import plotly.express as px
+
+df = px.data.iris()
+fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
+                                                              "sepal_width": "Sepal Width",
+                                                              "sepal_length": "Sepal Length",
+                                                              "petal_width": "Petal Width",
+                                                              "petal_length": "Petal Length", },
+                              color_continuous_scale=px.colors.diverging.Tealrose,
+                              color_continuous_midpoint=2)
+st.plotly_chart(fig)
--- util/meta_info/demo_meta_info_data_generate.py
+++ util/meta_info/demo_meta_info_data_generate.py
@@ -1,3 +1,43 @@
 # Author: ray
 # Date: 3/25/24
 # Description:
+import json
+import os.path
+
+import numpy as np
+
+from util.common import get_proje_root_path
+
+proje_root = get_proje_root_path()
+meta_info_folder = os.path.join(proje_root, "statistic/meta_info")
+
+if not os.path.exists(meta_info_folder):
+    os.makedirs(meta_info_folder)
+    print(f"Directory '{meta_info_folder}' was created.")
+else:
+    print(f"Directory '{meta_info_folder}' already exists.")
+
+input_list = list(range(96, 5000, 96))
+label_list = list(range(48, 5000, 48))
+pred_list = list(range(96, 5000, 96))
+
+counter = 0
+meta_info_list = []
+
+for i in input_list:
+    for j in label_list:
+        for k in pred_list:
+            mse = np.random.uniform(0.001, 5)
+            mae = np.random.uniform(0.001, 5)
+            meta_info_list.append(
+                {"input_length": i,
+                 "label_length": j,
+                 "predict_length": k,
+                 "mse": mse,
+                 "mae": mae,
+                 "model_id": f"testing_model_{counter}"}
+            )
+            counter += 1
+
+with open(os.path.join(meta_info_folder, "test_meta_info.json"), "w") as f:
+    json.dump(meta_info_list, f, indent=4)
