6a4d1a441f04992ef6205a61b7893a59bb693589
Sirui Li <slray@nyu.edu>
2024-03-29 23:49:04 +0400

    testing resync

--- util/auto_sync_hpc_file.py
+++ util/auto_sync_hpc_file.py
@@ -13,18 +13,21 @@ load_dotenv()
 hpc_user_name = os.getenv('HPC_USERNAME')
 
 
-def sync_hpc_files():
+def sync_hpc_files(source_path: str = "/scratch/sl9625/FEDformer_meta_script/scripts/"):
+    # /scratch/sl9625/FEDformer_meta_script/scripts/
     # Define the rsync command
     rsync_command = [
         "rsync",
         "-avz",
         "--partial",
-        "--progress", # TODO: change here
+        "--progress",  # TODO: change here
         "-e", "ssh -i ~/.ssh/id_ed25519",  # Specify the path to your SSH private key
-        f"{hpc_user_name}:/scratch/sl9625/",  # Replace with your HPC username, hostname, and source directory
-        "/Users/ray/rayfile/self-project/research_ml_visualization"  # Replace with your local destination directory
+        f"{hpc_user_name}:{source_path}",
+        # Replace with your HPC username, hostname, and source directory
+        "/Users/ray/rayfile/self-project/research_ml_visualization/testing_resync"
+        # Replace with your local destination directory
     ]
-
+    print(f"command: {rsync_command}")
     # Execute the rsync command
     result = subprocess.run(rsync_command, capture_output=True, text=True)
 
@@ -38,4 +41,7 @@ def sync_hpc_files():
 
 
 if __name__ == "__main__":
-    sync_hpc_files()
+    source_folder = "/scratch/sl9625/FEDformer_meta_script"
+    to_sync_folders = ["checkpoints", "meta_info", "results"]
+    source_paths = [os.path.join(source_folder, to_sync_folder) for to_sync_folder in to_sync_folders]
+    [sync_hpc_files(source_path) for source_path in source_paths]
