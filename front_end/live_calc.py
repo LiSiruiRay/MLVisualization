3be1198fa27b6abced23d43c2a1c1c1434eb4530
Sirui Li <slray@nyu.edu>
2024-05-20 22:46:44 +0400

    multi-model test passed

--- front_end/live_calc.py
+++ front_end/live_calc.py
@@ -5,6 +5,7 @@ import json
 import sys
 import os.path
 from datetime import date
+from typing import List
 
 import pandas as pd
 
@@ -25,7 +26,9 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(leve
 logger = logging.getLogger(__name__)
 
 
-def live_calc_output(meta_info: dict, ml: ModelLoader, dpl: DataProviderLoader, fig):
+def live_calc_output(ml_list: List[ModelLoader], dpl_list: List[DataProviderLoader], fig):
+
+    meta_info = ml_list[0].meta_info
     training_data = read_training_ds_by_meta(meta_info=meta_info)
 
     input_length = meta_info['seq_len']
@@ -54,13 +57,16 @@ def live_calc_output(meta_info: dict, ml: ModelLoader, dpl: DataProviderLoader, 
     input_for_test = selected_sub_frame.iloc[0:input_length + pred_length]  # data_x and data_y
 
     if st.button("calculate"):
-        update_fig_to_show_pred_meta_info(window_start_point=0,
-                                          meta_info=meta_info,
-                                          selected_sub_frame=selected_sub_frame,
-                                          fig=fig,
-                                          input_data=input_for_test,
-                                          ml=ml,
-                                          dpl=dpl)
+        for i, ml in enumerate(ml_list):
+            dpl = dpl_list[i]
+            update_fig_to_show_pred_meta_info(window_start_point=0,
+                                              meta_info=meta_info,
+                                              selected_sub_frame=selected_sub_frame,
+                                              fig=fig,
+                                              input_data=input_for_test,
+                                              ml=ml,
+                                              dpl=dpl,
+                                              model_number=i)
 
     st.plotly_chart(fig)
 
@@ -70,20 +76,23 @@ def update_fig_to_show_pred_meta_info(window_start_point: int,
                                       selected_sub_frame: pd.DataFrame,
                                       fig,
                                       input_data: pd.DataFrame,
-                                      ml: ModelLoader, dpl: DataProviderLoader):
+                                      ml: ModelLoader, dpl: DataProviderLoader,
+                                      model_number):
     update_fig_to_show_pred_detailed(window_start_point=window_start_point,
                                      seq_len=meta_info["seq_len"],
                                      pred_len=meta_info["pred_len"],
                                      selected_sub_frame=selected_sub_frame,
                                      fig=fig, input_data=input_data,
-                                     ml=ml, dpl=dpl)
+                                     ml=ml, dpl=dpl,
+                                     model_number=model_number)
 
 
 def update_fig_to_show_pred_detailed(window_start_point: int,
                                      seq_len: int, pred_len: int,
                                      selected_sub_frame: pd.DataFrame, fig,
                                      input_data: pd.DataFrame,
-                                     ml: ModelLoader, dpl: DataProviderLoader):
+                                     ml: ModelLoader, dpl: DataProviderLoader,
+                                     model_number):
     pred = ml.predict(input_data=input_data, dpl=dpl)
     pred_series = pd.Series(pred.flatten())
     prediction_dates = selected_sub_frame['date'][window_start_point + seq_len: window_start_point + seq_len + pred_len]
@@ -94,7 +103,7 @@ def update_fig_to_show_pred_detailed(window_start_point: int,
         x=prediction_dates,
         y=prediction_values,
         mode='lines',
-        name='Predicted Data',
+        name=f'Predicted Data Model: {model_number}',
         line=dict(color='orange')
     ))
     fig.update_layout(
@@ -128,20 +137,30 @@ def update_fig_to_show_test(fig, selected_sub_frame, window_start_point, window_
                   fillcolor="LightSkyBlue", opacity=0.5,
                   name="selected part")
 
-    # fig.update_layout(
-    #     title='Time Series Plot with Selection Window',
-    # )
-
 
 if __name__ == '__main__':
     print(f"hello")
     model_id = "pure_sin_first_with_meta_script_20240330@03h10m11s_20240330@03h10m11s"
+    model_id_2 = "pure_sin_first_with_meta_script_20240401@03h16m24s_20240401@03h16m24s"
+
     fig = go.Figure()
+
     fig.update_layout(
         title='Time Series Plot with Selection Window',
     )
     ml = ModelLoader(model_id=model_id)
+    ml_2 = ModelLoader(model_id=model_id_2)
+
     ml.load_model()
+    ml_2.load_model()
+
     dpl = DataProviderLoader(model_id=model_id)
+    dpl_2 = DataProviderLoader(model_id=model_id_2)
+
     dpl.load_load_data_provider()
-    live_calc_output(ml.meta_info, ml=ml, dpl=dpl, fig=fig)
+    dpl_2.load_load_data_provider()
+
+    ml_list = [ml, ml_2]
+    dpl_list = [dpl, dpl_2]
+    live_calc_output(ml_list=ml_list, dpl_list=dpl_list, fig=fig)
+    # live_calc_output(ml_2.meta_info, ml=ml_2, dpl=dpl_2, fig=fig)
